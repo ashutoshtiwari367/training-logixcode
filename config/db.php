37c530bff2a50717892cd1f3fd692ae783c71e1c
ashutoshtiwari367 <ashutoshtiwari9453@gmail.com>
2026-05-23 15:28:41 +0530

    add

--- config/db.php
+++ config/db.php
@@ -21,7 +21,7 @@ if ($httpHost === 'localhost' || $httpHost === '127.0.0.1' || strpos($requestUri
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'u447123054_institute_regi');
     define('DB_USER', 'u447123054_institute_regi');
-    define('DB_PASS', 'Mu$k@n1106');
+    define('DB_PASS', 'Logixcode@2024');
     define('BASE_URL', '/');
 }
 
